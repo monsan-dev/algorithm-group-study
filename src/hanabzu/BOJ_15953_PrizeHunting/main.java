bdd7841d2b9a323d32bcf9a6020f5f85831c42f2
hanabzu <hanthird@gmail.com>
2021-05-03 02:08:17 +0900

    edit to more adequate camelcase

--- src/hanabzu/BOJ_15953_PrizeHunting/main.java
+++ src/hanabzu/BOJ_15953_PrizeHunting/main.java
@@ -1,28 +1,28 @@
 
-package hanabzu.BOJ_15952_PrizeHunting;
+package hanabzu.BOJ_15953_PrizeHunting;
 import java.util.Scanner;
 
 class PrizeTable{
-    private int[] prize_1 = {5000000,3000000,2000000,500000,300000,100000};
-    private int[] prize_2 = {5120000,2560000,1280000,640000,320000};
+    private int[] prize2017 = {5000000,3000000,2000000,500000,300000,100000};
+    private int[] prize2018 = {5120000,2560000,1280000,640000,320000};
 
-    public int getPrize_1(int rank){
+    public int getPrize2017(int rank){
         if(rank<22 && rank>0){
             for(int i=1;i<7;i++){
                 rank-=i;
                 if (rank<=0){
-                    return prize_1[i-1];
+                    return prize2017[i-1];
                 }
             }
         }
         return 0;
     }
-    public int getPrize_2(int rank){
+    public int getPrize2018(int rank){
         if(rank<32 && rank>0){
             for(int i=1;i<6;i++){
                 rank=rank>>1;
                 if(rank<1){
-                    return prize_2[i-1];
+                    return prize2018[i-1];
                 }
             }
         }
@@ -37,7 +37,7 @@ public class main{
         int prize;
         PrizeTable pt = new PrizeTable();
         for(int i=0; i<n; i++){
-            prize= pt.getPrize_1(sc.nextInt())+pt.getPrize_2(sc.nextInt());
+            prize= pt.getPrize2017(sc.nextInt())+pt.getPrize2018(sc.nextInt());
             System.out.println(prize);
         }
         sc.close();
